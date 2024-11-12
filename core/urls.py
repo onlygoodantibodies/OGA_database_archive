27686bb40e181f834a54a98a91aad867e14ff1b4
barbaraferreira <barbaratferreira77@gmail.com>
2024-11-12 14:27:21 +0000

    Initial commit

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+from . import views  
+
+urlpatterns = [
+    path('', views.home, name='home'),  # Home page
+    path('about/', views.about, name='about'),  # About Us page
+    path('projects/', views.projects, name='projects'),  # Projects page
+    path('data/', views.data_view, name='data'), #Data Page
+    path('partners/', views.partners, name='partners'),  # Partners page
+    path('contact/', views.contact, name='contact'), #Contact page
+    path('success/', views.success, name='success'), #Success Pgae
+    path('news/', views.news, name='news'), # News Page
+]
---
+++ core/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render, redirect
+
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def projects(request):
+    return render(request, 'core/projects.html')
+
+def publications(request):
+    return render(request, 'core/publications.html')
+
+def partners(request):
+    return render(request, 'core/partners.html')
+
+def data_view(request):
+    return render(request, 'core/data.html') 
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        subject = f'Contact Form Submission from {name}'
+        full_message = f'Name: {name}\nEmail: {email}\n\nMessage:\n{message}'
+
+        send_mail(
+            subject,
+            full_message,
+            email,
+            [settings.EMAIL_HOST_USER],
+            fail_silently=False,
+        )
+        return redirect('success')  # Redirect to the success page
+    
+    return render(request, 'core/contact.html')
+
+def success(request):
+    return render(request, 'core/success.html')
+
+def news(request):
+    return render(request, 'core/news.html')
