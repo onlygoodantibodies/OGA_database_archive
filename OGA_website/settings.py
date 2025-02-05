17264ac8c5b50b5a8daaba47ffbec241f4ed49fd
barbaraferreira <barbaratferreira77@gmail.com>
2025-02-05 10:26:32 +0000

    Chnages in allowed hosts

--- OGA_website/settings.py
+++ OGA_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mb78)#ej_8g=30ain=bx$k#15=c0kw(^ircfzudzkqzyy_yq90
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['onlygoodantibodies.com', 'only-good-antibodies.onrender.com', '127.0.0.1', 'https://oga-website.onrender.com']
+ALLOWED_HOSTS = ['onlygoodantibodies.com', 'only-good-antibodies.onrender.com', '127.0.0.1', 'oga-website.onrender.com']
 
 
 # Application definition
